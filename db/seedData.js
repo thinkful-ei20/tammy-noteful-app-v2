aa791c191812c8eaeacd2f4052c1da3be9982cba
Tammy Tian <tiantammy@outlook.com>
2018-04-28 04:43:14 +0800

    added travis badge

--- db/seedData.js
+++ db/seedData.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const knex = require('../knex');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
-module.exports = function(file, user = 'dev') {
+module.exports = function(file) {
   return exec(`psql -f ${file} -d postgres://postgres:@localhost/noteful-test`);
 };
