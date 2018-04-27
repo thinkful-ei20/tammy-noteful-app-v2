96486010a2b31b89b79f01288452eb895f51998f
Tammy Tian <tiantammy@outlook.com>
2018-04-28 03:22:34 +0800

    debugging travis

--- knex.js
+++ knex.js
@@ -4,4 +4,6 @@ const knexConfig = require('./knexfile');
 
 const environment = process.env.NODE_ENV || 'development';
 
+console.log(knexConfig[environment]);
+
 module.exports = require('knex')(knexConfig[environment]);
