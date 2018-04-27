238136f26cee7b5c0010666619e134833b6988ad
Tammy Tian <tiantammy@outlook.com>
2018-04-28 03:54:03 +0800

    debugging Travis

--- db/seedData.js
+++ db/seedData.js
@@ -5,5 +5,5 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 module.exports = function(file, user = 'dev') {
-  return exec(`psql -f ${file} -d postgres://dev:dev@localhost/noteful-test`);
+  return exec('psql -f C:/Users/tiant/projects/thinkful/noteful-app-v2/db/noteful.sql -d postgres://dev:dev@localhost/noteful-test');
 };
